90fb942d3db694ade43de039c2a17d6829c71abe
joaog <joaog@LAPTOP-7251LD7D>
2024-05-14 18:21:30 -0300

    Atualização

---
+++ Administrativo.CCB.Dominio/Entity/Usuario.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Administrativo.CCB.Dominio.Entity
+{
+    public class Usuario : EntityBase.EntityBase
+    {
+        public string NomeUsuario { get; set; }
+        public string Senha { get; set; }
+        public string ConfirmaSenha { get; set; }
+    }
+}
